19450b57bae781ed3c953bc5f0441b72f78356f3
kboeff <kamen.boev@gmail.com>
2021-03-07 23:55:04 +0200

    minor additions to login

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,64 @@
-import React, {forwardRef, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
-
-const submitLogin = () => {};
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [data, setData] = useState({
+    loading: false,
+    loadedData: {},
+  });
+
+  const submitLogin = () => {
+    fetch('http://localhost:3000/')
+      .then((res) => res.json())
+      .then(console.log);
+  };
 
   return (
-    <View style={styles.loginContainer}>
-      <Text>Email</Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <Text style={styles.passwordText}>Password</Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
-      <Button
-        style={styles.button}
-        onPress={submitLogin}
-        title="Submit"
-        color="#841584"
-        accessibilityLabel="Logs you in"
-      />
-    </View>
+    (data.loading && (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    )) || (
+      <View style={styles.loginContainer}>
+        <Text>Email</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <Text style={styles.passwordText}>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+        <Button
+          style={styles.button}
+          onPress={submitLogin}
+          title="Submit"
+          color="#841584"
+          accessibilityLabel="Logs you in"
+        />
+      </View>
+    )
   );
 };
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   loginContainer: {
     display: 'flex',
     flexDirection: 'column',
